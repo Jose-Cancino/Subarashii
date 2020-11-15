ab7ca5185ed73c73022d72657c74248dc2308193
Jose-Cancino <josemartin091101@gmail.com>
2020-11-15 15:41:06 -0300

    Create App.js

---
+++ desafios/desafio_n1/App.js
@@ -0,0 +1,80 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState, Component} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  const [usuario, setUsuario] = React.useState("Usuario");
+  const [contraseña, setContraseña] = React.useState("Contraseña");
+  const [mensaje, setMensaje] = React.useState("")
+
+  function comprobar(){
+    if (usuario === "Pabst" && contraseña === "neko"){
+      setMensaje("Wena po")
+    } else{
+      setMensaje("No Sos VOS")
+      Alert.alert("Quien Sos?"), [
+        {text: "soy yoh"}
+      ];
+    }
+
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>Iniciar Sesión</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder = "Usuario"
+        onChangeText = {(val) => setUsuario(val)}
+        />
+      <TextInput 
+        style={styles.input}
+        placeholder = "Contraseña"
+        onChangeText = {(val) => setContraseña(val)}
+        />
+      <TouchableOpacity onPress={() => comprobar()}>
+        <Text style={styles.boton}> Iniciar Sesión </Text> 
+      </TouchableOpacity>
+      <Text style={styles.texto}> {mensaje} </Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'cyan',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: "center",
+  
+  },
+  texto: {
+    fontSize: 20,
+    fontFamily: "monospace"
+  
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200,
+    backgroundColor: "white",
+
+  },
+  boton: {
+    borderWidth: 1,
+    height: 42,
+    width:200,
+    borderRadius: 24,
+    textAlignVertical: "center",
+    alignItems: "center",
+    textAlign: "center",
+    backgroundColor: "white",
+  },
+});
+
+
+
