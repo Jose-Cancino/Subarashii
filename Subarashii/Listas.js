f17ee43d0518bf805ba9a8fdc032de804dab8d53
Joaquin Pabst <jpabst@alumnos.inf.utfsm.cl>
2020-11-23 23:46:59 -0300

    Create Listas.js

---
+++ Subarashii/Listas.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Recomendacion from "./Recomendacion"
+import Busqueda from "./Busqueda"
+
+
+
+
+
+const Listas= () => {
+    return(
+    <Text>
+        Listas
+
+    </Text>
+       
+        
+
+    );
+
+}
+export default Listas;
