1066498fefc9b4028e0bc00f5e2f99b03165cc3d
Joaquin Pabst <jpabst@alumnos.inf.utfsm.cl>
2020-11-23 23:46:39 -0300

    Create App.js

---
+++ Subarashii/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Recomendacion from "./Recomendacion"
+import Lista from "./Listas"
+import Busqueda from "./Busqueda"
+
+
+
+const Tab = createBottomTabNavigator();
+
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Tab.Navigator> 
+      <Tab.Screen name="Lupita" component={Busqueda} />
+      <Tab.Screen name="Home" component={Recomendacion} />
+      <Tab.Screen name="lista" component={Lista} />
+    </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
