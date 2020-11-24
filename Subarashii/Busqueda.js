148480ba96082ef6f68bb02f7093507ed8ed92ff
Joaquin Pabst <jpabst@alumnos.inf.utfsm.cl>
2020-11-23 23:46:56 -0300

    Create Busqueda.js

---
+++ Subarashii/Busqueda.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Recomendacion from "./Recomendacion"
+import Lista from "./Listas"
+
+
+
+
+
+const Busqueda= () => {
+    return(
+    <Text>
+        nyaaaaa
+
+    </Text>
+       
+        
+
+    );
+
+}
+export default Busqueda;
