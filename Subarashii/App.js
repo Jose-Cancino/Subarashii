5f9c8a4a43327373388492f1d4fbc146cbb89374
Joaquin Pabst <jpabst@alumnos.inf.utfsm.cl>
2020-11-25 20:56:38 -0300

    Update App.js

--- Subarashii/App.js
+++ Subarashii/App.js
@@ -18,7 +18,7 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer >
-    <Tab.Navigator> 
+    <Tab.Navigator initialRouteName = "Home"> 
       <Tab.Screen name="Lupita" component={BusquedaAnime} />
       <Tab.Screen name="Home" component={Recomendacion} />
       <Tab.Screen name="lista" component={Lista} />
