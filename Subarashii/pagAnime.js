9f5dfbe0b8bd36f2218b87a3e40e138d6b1a32c4
Joaquin Pabst <jpabst@alumnos.inf.utfsm.cl>
2020-11-25 18:35:53 -0300

    pagAnime 1.5

--- Subarashii/pagAnime.js
+++ Subarashii/pagAnime.js
@@ -1,70 +1,97 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, Button, FlatList} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { 
+  StyleSheet,
+  Text,
+  View, 
+  SafeAreaView,
+  ActivityIndicator,
+  Image,
+  Button
+ } from 'react-native';
 
 
 
 
 
-const pagAnime = ( {navigation, route}) => {
-    return(
-    
-    <SafeAreaView style={styles.constainer}>
+const pagAnime = ({navigation, route}) => {
+  const [isLoading, setLoading] = useState(true);
+  const [foto, setFoto] = useState("")
+  const [title, setTitle] = useState("")
+  const [synopsis, setSynopsis] = useState("")
+  const animeURL = `https://kitsu.io/api/edge/anime/${route.params.id}`
+  const [rating, setRating] = useState("")
+  
 
-        <Text style={styles.title}>{route.params.canonico}</Text>
-        <Image 
+  useEffect (() => {
+    fetch(animeURL)
+    .then((response) => response.json())
+    .then((json) => {
+      setFoto(json.data.attributes.posterImage.small);
+      setTitle(json.data.attributes.canonicalTitle);
+      setSynopsis(json.data.attributes.synopsis);
+      setRating(json.data.attributes.averageRating);
 
-        source = {{uri: route.params.imagen}}
-        style={{
-          
-          width:210,
-          height:300,
-          borderWidth:2,
-          borderColor:'#d35647',
-          
-          resizeMode:'contain',
-          margin:8
-        }}
+    })
+    .catch((error) => alert(error))
+    .finally(setLoading(false));  
+  },[]);
+   
+  return (
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (
+      <ActivityIndicator/> 
+      ):( 
+      <View >
+        
+      <Text style= {styles.title}> {title} </Text> 
+      <Image style={styles.imagen}
+        source = {{uri: foto}}
         />
+      <Text>
+        {synopsis}
+        
+      </Text>  
 
-        <Text style={styles.parrafo} >{route.params.desc}</Text>
+      <Text>
+        {rating}
 
-        <Button
-            title = "Agregar"
-            
+      </Text>
+      <Button
+      title = "Añadir"
+      
+      
+      
+      ></Button>
 
+      </View>
+      )}
+    </SafeAreaView>
+  );
+}
 
-        ></Button>
-    </SafeAreaView>   
-    
-    );
-  }
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontSize:20,  
-      height: 40,
-      lineHeight: 35,
-      textAlign: "center",        
-    },
-    item: {
-        backgroundColor: '#f9c2ff',
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-      }, 
-      parrafo: {
-        fontSize:12,  
-     
-      },
-
-    
-
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imagen:{
+    width:210,
+    height:300,
+    borderWidth:2,
+    borderColor:'#d35647',
+    resizeMode:'contain',
+    alignItems: "center",
+    margin:8
+  },
+  title: {
+    fontSize:20,  
+    height: 40,
+    lineHeight: 35,
+    textAlign: "center",        
+  },
+});
 
 export default pagAnime;
