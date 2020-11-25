752cf1df75c5e29804deab3f6133a4d1744fd4ec
Joaquin Pabst <jpabst@alumnos.inf.utfsm.cl>
2020-11-24 23:53:40 -0300

    Update Busqueda.js

--- Subarashii/Busqueda.js
+++ Subarashii/Busqueda.js
@@ -1,115 +1,32 @@
+
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, Component } from 'react';
+import React, { useState } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Button, Text, View, TextInput, Alert, SafeAreaView, KeyboardAvoidingView, } from 'react-native';
 
-import { 
-   StyleSheet,
-   Button,
-   Text,
-   View,
-   Image,
-   TextInput,
-   FlatList,
-   ActivityIndicator,
-   SafeAreaView,
-   TouchableOpacity,
-   } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
-import Recomendacion from "./Recomendacion"
-import Lista from "./Listas"
+import Busqueda from "./buscar" 
+import pagAnime from "./pagAnime" 
+
+const Stack = createStackNavigator();
 
 
-
-
-
-
-
-export default function App() {
-
-  const [datos, setDatos] = useState("")
-  const animeAPI = `https://kitsu.io/api/edge/anime?filter[text]=${datos}`;
-  
-
-  const [isLoading, setLoading] = useState(true)
-  const [lista, setLista] = useState([])
-  
-
-  
-  const Buscar = () => {
-      fetch(animeAPI)
-        .then((response) => response.json())
-        .then((json) => {
-          setLista(json.data);
-        })
-        .catch((error) => alert(error))
-        .finally(() => setLoading(false));
-
-    };
-  
-  
-const Item = ({ title, imagen }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-    <Image 
-    source = {{uri: imagen}}
-    style={{
-      width:210,
-      height:300,
-      borderWidth:2,
-      borderColor:'#d35647',
-      resizeMode:'contain',
-      margin:8
-    }}
-    />
-  </View>
-);
-
-  
-
-  const renderItem = ({item}) => (
-      <Item 
-       title={item.attributes.slug} 
-       imagen={item.attributes.posterImage.small} 
-       />
-  );
-
-
-
-
+const BusquedaAnime= () => {
   return (
-    <SafeAreaView>
-      <TextInput
-        style = {styles.input}
-        onChangeText = {text => setDatos(text)}
-        value = {datos}
-        placeholder = "Buscar..."
-      ></TextInput>
-      <Button
-      title = "buscar"
-      onPress = {() => Buscar()  }
-      ></Button>
-    
-      {isLoading ? ( 
-        <ActivityIndicator/> 
-      ) : ( 
-        <FlatList
-          data = {lista} 
-          keyExtractor = {({ data }, index) => data}
-          renderItem = {renderItem}
-        
-            
-        /> 
-      )}
-    </SafeAreaView>
+      
+        <Stack.Navigator initialRoutName = "Buscar">
+          <Stack.Screen name= "Buscar" component = {Busqueda} />
+          <Stack.Screen name= "Anime" component = {pagAnime} />
+          
+
+        </Stack.Navigator>
+      
   );
-  
-};
-
-
-
-
-
+}
 
 
 
@@ -124,11 +41,7 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: 'gray',
     borderWidth: 1 
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },  
+
+  }
 });
+export default BusquedaAnime;
