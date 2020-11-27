a9066608b6b3d9bfd8556be757d04349e4d461b3
Jose-Cancino <josemartin091101@gmail.com>
2020-11-26 21:53:54 -0300

    Update Listas.js

--- Subarashii/Listas.js
+++ Subarashii/Listas.js
@@ -1,26 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
+import React, {useState, Component} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image, Button, TextInput, FlatList, TouchableNativeFeedback, TouchableOpacity} from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import ListaRec from './ListaRec';
 
-import Recomendacion from "./Recomendacion"
-import Busqueda from "./Busqueda"
-
-
-
-
-
-const Listas= () => {
-    return(
-    <Text>
-        Listas
-
-    </Text>
-       
-        
-
-    );
-
+const Lista = ({navigation}) => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Button 
+                title= "Recomendación del programador"
+                onPress={() => navigation.navigate("Lista Recomendada")}>
+            </Button>
+        </SafeAreaView>
+    )
 }
-export default Listas;
+
+const Stack = createStackNavigator();
+function NListas() {
+    return (
+        <Stack.Navigator initialRouteName = "Listas">
+            <Stack.Screen name = "Listas" component = {Lista} />
+            <Stack.Screen name = "Lista Recomendada" component = {ListaRec} />
+        </Stack.Navigator>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+export default NListas;
