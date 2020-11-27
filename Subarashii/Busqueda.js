2cf2b8f983c8592605289254078a2e1220655d0c
Jose-Cancino <josemartin091101@gmail.com>
2020-11-26 21:53:48 -0300

    Update Busqueda.js

--- Subarashii/Busqueda.js
+++ Subarashii/Busqueda.js
@@ -1,19 +1,11 @@
-
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Button, Text, View, TextInput, Alert, SafeAreaView, KeyboardAvoidingView, } from 'react-native';
-
-import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Header } from 'react-native/Libraries/NewAppScreen';
-
 import Busqueda from "./buscar" 
 import pagAnime from "./pagAnime" 
 
 const Stack = createStackNavigator();
-
 
 const BusquedaAnime= () => {
   return (
@@ -21,14 +13,9 @@ const BusquedaAnime= () => {
         <Stack.Navigator initialRoutName = "Buscar">
           <Stack.Screen name= "Buscar" component = {Busqueda} />
           <Stack.Screen name= "Anime" component = {pagAnime} />
-          
-
-        </Stack.Navigator>
-      
+        </Stack.Navigator>  
   );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
