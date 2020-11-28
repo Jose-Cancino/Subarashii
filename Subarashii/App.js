0f3da57d68d8600bac9927deeb1de4345def0e63
Jose-Cancino <josemartin091101@gmail.com>
2020-11-28 01:44:50 -0300

    Update App.js

--- Subarashii/App.js
+++ Subarashii/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import Icon from "react-native-ionicons"
 
 import Recomendacion from "./Recomendacion"
 import Lista from "./Listas"
@@ -12,12 +14,12 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer >
-    <Tab.Navigator initialRouteName = "Home"> 
+    <Tab.Navigator 
+      initialRouteName = "Home"> 
       <Tab.Screen name="Lupita" component={BusquedaAnime} />
-      <Tab.Screen name="Home" component={Recomendacion} />
-      <Tab.Screen name="lista" component={Lista} />
+      <Tab.Screen name="Home" component={Recomendacion}/>
+      <Tab.Screen name="Listas" component={Lista}/>
     </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
