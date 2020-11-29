9f66720bab8fdc13a4ef09cfc0642ea780bff2c9
Joaquin Pabst <jpabst@alumnos.inf.utfsm.cl>
2020-11-29 20:49:36 -0300

    Update Lista10.js

--- Subarashii/Listas/Lista10.js
+++ Subarashii/Listas/Lista10.js
@@ -1,7 +1,97 @@
-import React from "react"
-import { Text } from "react-native"
-export default function Lista1(){
+import React, { useState } from "react";
+import { 
+    StyleSheet, 
+    Text, 
+    View, 
+    TouchableNativeFeedback,
+    TouchableOpacity,
+    ScrollView
+  } from 'react-native';
+
+
+
+
+
+const Lista10 = ({navigation}) => {
+    const [anime, setAnime] = useState([
+        {name: "12243"},
+        {name: "3936"},
+        {name: "6448"},
+        {name: "1376"},
+        {name: "7158"},
+        {name: "1801"},
+        {name: "7442"},
+        {name: "41440"},
+        {name: "1415"},
+        {name: "5853"},
+        {name: "3919"},
+        {name: "7711"},
+        {name: "4604"},
+        {name: "5981"},
+        {name: "10028"},
+        {name: "11614"},
+        //{name: "41995"},
+        //{name: "7821"},
+        //{name: "831"},
+        //{name: "7712"},
+        //{name: "4240"},
+        //{name: "7160"},
+        //{name: "8133"},
+        //{name: "7023"},
+        //{name: "6028"}
+      ]);
+    
+    
+
+
+    const Bloques = anime.map(anime => {
+      const [nombre, setNombre] = useState("")
+      const [id, setId] = useState("")
+  
+          fetch(`https://kitsu.io/api/edge/anime/${anime.name}`)
+          .then((response) => response.json())
+          .then((json) => {
+            setNombre(json.data.attributes.canonicalTitle);
+            setId(json.data.id)
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+  
+        return ( 
+          <View >
+            <TouchableOpacity onPress = {() => navigation.navigate("Anime", {id: `${id}`})}>
+              <Text style={styles.lista}>{nombre}</Text>
+            </TouchableOpacity>
+          </View>
+          
+          
+          );
+        });
+
     return(
-    <Text> Lista 10 </Text>
-    )
+    <ScrollView style ={styles.scrollView}>
+
+        <View >
+        {Bloques}
+        </View>
+
+    </ScrollView>
+    );
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    lista:{
+      marginHorizontal: 20,
+      marginTop: 24,
+      padding: 30,
+      backgroundColor: "pink",
+      fontSize: 24,
+    },
+  });
+export default Lista10;
