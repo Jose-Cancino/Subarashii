593616d9790a83706cf0ea9cc7036f70ec2ee8af
Javiera9920 <74120075+Javiera9920@users.noreply.github.com>
2020-11-30 23:03:42 -0300

    Update pagAnime.js

--- Subarashii/pagAnime.js
+++ Subarashii/pagAnime.js
@@ -7,7 +7,10 @@ import {
   ActivityIndicator,
   Image,
   Button,
-  ScrollView
+  ScrollView,
+  Linking,
+  TouchableOpacity,
+  FlatList
  } from 'react-native';
 
 const pagAnime = ({navigation, route}) => {
@@ -16,7 +19,11 @@ const pagAnime = ({navigation, route}) => {
   const [title, setTitle] = useState("")
   const [synopsis, setSynopsis] = useState("")
   const animeURL = `https://kitsu.io/api/edge/anime/${route.params.id}`
+  const categoriaUrl = `https://kitsu.io/api/edge/anime/${route.params.id}/categories`
   const [rating, setRating] = useState("")
+  const [lista, setLista] = useState([])
+  const [video, setVideo] = useState('')
+
   
   useEffect (() => {
     fetch(animeURL)
@@ -26,11 +33,29 @@ const pagAnime = ({navigation, route}) => {
       setTitle(json.data.attributes.canonicalTitle);
       setSynopsis(json.data.attributes.synopsis);
       setRating(json.data.attributes.averageRating);
-
+      setVideo(json.data.attributes.youtubeVideoId);
+    })
+    fetch(categoriaUrl)
+    .then((response) => response.json())
+    .then((json) => {
+      setLista(json.data)
     })
     .catch((error) => alert(error))
     .finally(setLoading(false));  
   },[]);
+  
+  const Item = ({ categoria }) => (
+  
+    <View style={styles.item}>  
+      <Text>{categoria}</Text>
+    </View>
+  );
+
+  const renderItem = ({item}) => (
+    <Item 
+     categoria = {item.attributes.title} 
+     />
+  );
    
   return (
     <SafeAreaView style={styles.container}>
@@ -44,26 +69,33 @@ const pagAnime = ({navigation, route}) => {
       <Image style={styles.imagen}
         source = {{uri: foto}}
         />
-      <Text>
+      <Text style= {styles.sinopsis}>
         {synopsis}
-        
+
       </Text>  
 
-      <Text style={styles.title}>
+      <Text style={styles.rating}>
         Rating: {rating}
 
       </Text>
-      <Text>
-
-        
+      <Text style={styles.subtitle}>
+        CATEGORÍAS
       </Text>
-      <Button
-      title = "Añadir"
-      
-      
-      
-      ></Button>
-
+      <FlatList
+          data = {lista} 
+          keyExtractor = {({ data }, index) => data}
+          renderItem = {renderItem}    
+        /> 
+    <TouchableOpacity onPress = {() => Linking.openURL(`https://www.youtube.com/watch?v=${video}`)}>
+        <Text style={styles.yutu}>
+          Ver Trailer
+        </Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => navigation.navigate("Listas")}>
+      <Text style = {styles.añadir}>
+        Añadir
+      </Text>
+    </TouchableOpacity>
       </View>
       )}
       </ScrollView>
@@ -84,19 +116,68 @@ const styles = StyleSheet.create({
     borderWidth:2,
     borderColor:'#d35647',
     resizeMode:'contain',
-    alignItems: "center",
-    margin:8
+    alignItems: 'center',
+    margin:8,
+    marginHorizontal: 80,
   },
   title: {
-    fontSize:20,  
+    fontSize:20,
+    backgroundColor: '#fff',  
     height: 40,
     lineHeight: 35,
-    textAlign: "center",        
+    textAlign: 'center',      
+  },
+  subtitle: {
+    fontSize:20, 
+    color: 'black',
+    height: 40,
+    lineHeight: 35,
+    marginVertical: 10,
+    textAlign: "center",  
+    textDecorationLine: 'underline',
   },
   scrollView: {
-    backgroundColor: 'pink',
+    backgroundColor: 'white',
     marginHorizontal: 20,
   },
+  yutu: {
+    color: "blue",
+    fontSize: 15,  
+    height: 40,
+    lineHeight: 35,
+    textAlign: "center", 
+    textDecorationLine: "underline"       
+  },
+  añadir: {
+    backgroundColor: '#d1bcf5',
+    color: 'black',
+    fontSize:20,  
+    lineHeight: 35,   
+    padding: 7,
+    marginVertical: 10,
+    marginHorizontal: 0,
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  sinopsis: {
+    //backgroundColor: '#d1bcf5',
+    color: 'black',
+    fontSize:15,  
+    //padding: 10,
+    marginVertical: 10,
+    marginHorizontal: 10,
+    alignItems: 'center',
+    textAlign: 'justify',
+  },  
+  rating: {
+    //backgroundColor: '#d1bcf5',
+    color: '#dbb107',
+    fontSize:18,  
+    marginVertical: 10,
+    marginHorizontal: 10,
+    alignItems: 'center',
+    textAlign: 'justify',
+  },  
 });
 
 export default pagAnime;
