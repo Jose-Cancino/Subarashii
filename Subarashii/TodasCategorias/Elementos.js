23d8b2cad2dd406dc9ec78be9045473e01148712
Joaquin Pabst <jpabst@alumnos.inf.utfsm.cl>
2020-12-10 20:49:26 -0300

    Create Elementos.js

---
+++ Subarashii/TodasCategorias/Elementos.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { StyleSheet,
+    Button,
+    Text,
+    View,
+    Image,
+    TextInput,
+    FlatList,
+    ActivityIndicator,
+    SafeAreaView,
+    ScrollView,
+    TouchableOpacity,
+    StatusBar 
+ } from 'react-native';
+
+ const Elementos = ({navigation}) => {
+    return(
+
+        <Text>hola</Text>
+
+    )
+
+
+ }
+ export default Elementos;
