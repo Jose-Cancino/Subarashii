2605ae64df3158b7ca90bbab0b4a2e4f805d64f8
Joaquin Pabst <jpabst@alumnos.inf.utfsm.cl>
2020-12-10 20:49:28 -0300

    Create Escenario.js

---
+++ Subarashii/TodasCategorias/Escenario.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { StyleSheet,
+    Button,
+    Text,
+    View,
+    Image,
+    TextInput,
+    FlatList,
+    ActivityIndicator,
+    SafeAreaView,
+    ScrollView,
+    TouchableOpacity,
+    StatusBar 
+ } from 'react-native';
+
+ const Escenario = ({navigation}) => {
+    return(
+
+        <Text>hola</Text>
+
+    )
+
+
+ }
+ export default Escenario;
