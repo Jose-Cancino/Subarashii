f0306f17af1d7e4309c74061bc1b2b2b89940486
Joaquin Pabst <jpabst@alumnos.inf.utfsm.cl>
2020-12-10 13:27:44 -0300

    Update Recomendacion.js

--- Subarashii/Recomendacion.js
+++ Subarashii/Recomendacion.js
@@ -10,15 +10,7 @@ import Lista from "./Listas"
 import Busqueda from "./Busqueda"
 
 import ListaRec1 from "./ListasRec/ListasRec1"
-import ListaRec2 from "./ListasRec/ListasRec2"
-import ListaRec3 from "./ListasRec/ListasRec3"
-import ListaRec4 from "./ListasRec/ListasRec4"
-import ListaRec5 from "./ListasRec/ListasRec5"
-import ListaRec6 from "./ListasRec/ListasRec6"
-import ListaRec7 from "./ListasRec/ListasRec7"
-import ListaRec8 from "./ListasRec/ListasRec8"
-import ListaRec9 from "./ListasRec/ListasRec9"
-import ListaRec10 from "./ListasRec/ListasRec10"
+import express from "./express"
 import AnimeUff from './AnimesUff';
 
 const RecomendacionTab = ({navigation}) => {
@@ -33,40 +25,8 @@ const RecomendacionTab = ({navigation}) => {
                     Lista Recomendada 1</Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.listas}>
-                <Text onPress={() => navigation.navigate("Lista Recomendada 2")}>
-                    Lista Recomendada 2</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.listas}>
-                <Text onPress={() => navigation.navigate("Lista Recomendada 3")}>
-                    Lista Recomendada 3</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.listas}>
-                <Text onPress={() => navigation.navigate("Lista Recomendada 4")}>
-                    Lista Recomendada 4</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.listas}>
-                <Text onPress={() => navigation.navigate("Lista Recomendada 5")}>
-                    Lista Recomendada 5</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.listas}>
-                <Text onPress={() => navigation.navigate("Lista Recomendada 6")}>
-                    Lista Recomendada 6</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.listas}>
-                <Text onPress={() => navigation.navigate("Lista Recomendada 7")}>
-                    Lista Recomendada 7</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.listas}>
-                <Text onPress={() => navigation.navigate("Lista Recomendada 8")}>
-                    Lista Recomendada 8</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.listas}>
-                <Text onPress={() => navigation.navigate("Lista Recomendada 9")}>
-                    Lista Recomendada 9</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.listas}>
-                <Text onPress={() => navigation.navigate("Lista Recomendada 10")}>
-                    Lista Recomendada 10</Text>
+                <Text onPress={() => navigation.navigate("Lista Recomendada 1")}>
+                    Lista Recomendada 1</Text>
             </TouchableOpacity>
       </ScrollView>
     );
@@ -78,15 +38,7 @@ const RecomendacionTab = ({navigation}) => {
         <Stack.Screen name = "Recomendaciones" component = {RecomendacionTab} />
         <Stack.Screen name = "Lista Recomendada 1" component = {ListaRec1} />
         <Stack.Screen name = "Lista Recomendada" component = {ListaRec} />
-            <Stack.Screen name = "Lista Recomendada 2" component = {ListaRec2} />
-            <Stack.Screen name = "Lista Recomendada 3" component = {ListaRec3} />
-            <Stack.Screen name = "Lista Recomendada 4" component = {ListaRec4} />
-            <Stack.Screen name = "Lista Recomendada 5" component = {ListaRec5} />
-            <Stack.Screen name = "Lista Recomendada 6" component = {ListaRec6} />
-            <Stack.Screen name = "Lista Recomendada 7" component = {ListaRec7} />
-            <Stack.Screen name = "Lista Recomendada 8" component = {ListaRec8} />
-            <Stack.Screen name = "Lista Recomendada 9" component = {ListaRec9} />
-            <Stack.Screen name = "Lista Recomendada 10" component = {ListaRec10} />
+        <Stack.Screen name = "express" component = {express} />
 
 
             <Stack.Screen name = "Animes ufff" component = {AnimeUff} />
