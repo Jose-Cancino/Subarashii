5f611c31e7b1e8f7ff8a48b9b5825980000eb6c9
Jose-Cancino <josemartin091101@gmail.com>
2020-12-12 22:16:33 -0300

    Update Elementos.js

--- Subarashii/TodasCategorias/Elementos.js
+++ Subarashii/TodasCategorias/Elementos.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { StyleSheet,
-    Button,
     Text,
     View,
     Image,
@@ -12,113 +11,322 @@ import { StyleSheet,
     TouchableOpacity,
     StatusBar 
  } from 'react-native';
+ import { Button } from "react-native-elements"
 
  const Elementos = ({navigation}) => {
     return(
         <ScrollView style ={styles.scrollView}>
         <View>
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Action"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Accion"
             ></Button>
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Adventure"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Aventura"
             ></Button>
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Angst"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Angustia"
             ></Button>
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Anime Influenced"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Influenciado por anime"
             ></Button>
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Anthropomorphism"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Antropomorfismo"
             ></Button>
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Comedy"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Comedia"
             ></Button>
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Detective"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Detectives"
             ></Button> 
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Drama"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Drama"
             ></Button>   
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Ecchi"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Ecchi"
             ></Button>    
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Fantasy"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Fantasy"
             ></Button>  
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Ghost"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Ghost"
             ></Button>  
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Harem"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Harem"
             ></Button>
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Henshin"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Henshin"
             ></Button>    
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Horror"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Horror"
             ></Button>   
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Magic Girl"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Chicas Magicas"
             ></Button>   
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Mystery"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Misterio"
             ></Button>   
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Parasite"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Parasitos"
             ></Button> 
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "psychological"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Psychological"
             ></Button>  
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Romance"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Romance"
             ></Button>  
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Science fiction"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Science fiction"
             ></Button>  
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Super power"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Super Poderes"
             ></Button> 
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Supernatural"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Supernatural"
             ></Button> 
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Thriller"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Suspenso"
             ></Button> 
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Vampire"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Vampiros"
             ></Button> 
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Virtual Reality"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Virtual Reality"
             ></Button>
-            <Button style={styles.listas}
+            <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Zombie"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 10,
+                  }}
                 title = "Zombies"
             ></Button>
         </View>
