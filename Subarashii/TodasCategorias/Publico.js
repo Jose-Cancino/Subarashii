4ba7bc8939b5633d941b2229a906eb9815877c4c
Jose-Cancino <josemartin091101@gmail.com>
2020-12-12 22:16:39 -0300

    Update Publico.js

--- Subarashii/TodasCategorias/Publico.js
+++ Subarashii/TodasCategorias/Publico.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { StyleSheet,
-    Button,
     Text,
     View,
     Image,
@@ -12,30 +11,70 @@ import { StyleSheet,
     TouchableOpacity,
     StatusBar 
  } from 'react-native';
-
+ import { Button } from "react-native-elements"
  const Publico = ({navigation}) => {
     return(
 
         <ScrollView style ={styles.scrollView}>
         <View>
-            <Button style={styles.listas}
+            <Button
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Josei"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 15,
+                  }}
                 title = "Josei"
             ></Button>
-            <Button style={styles.listas}
+            <Button
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Kids"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 15,
+                  }}
                 title = "Children"
             ></Button>
-            <Button style={styles.listas}
+            <Button
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Seinen"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 15,
+                  }}
                 title = "Seinen"
             ></Button>
-            <Button style={styles.listas}
+            <Button
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Shoujo"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 15,
+                  }}
                 title = "Shoujo"
             ></Button>
-            <Button style={styles.listas}
+            <Button
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "Shounen"})}
+                type = "outline"
+                buttonStyle = {{
+                    backgroundColor:"white",
+                    marginHorizontal: 20,
+                    borderColor: "white",
+                    borderRadius: 10,
+                    marginVertical: 15,
+                  }}
                 title = "Shounen"
             ></Button>
         </View>
