c09d53ec5dd12d15fedb04899762360f4c8c602b
Jose-Cancino <josemartin091101@gmail.com>
2020-12-21 15:56:34 -0300

    Update Lista9.js

--- Subarashii/Listas/Lista9.js
+++ Subarashii/Listas/Lista9.js
@@ -1,7 +1,197 @@
-import React from "react"
-import { Text } from "react-native"
-export default function Lista1(){
-    return(
-    <Text> Lista 9 </Text>
-    )
+import React, {useState, Component, useEffect} from 'react';
+import { StyleSheet, Text, View, TextInput, Alert, SafeAreaView, ScrollView, TextPropTypes } from 'react-native';
+import AsyncStorage from "@react-native-community/async-storage";
+import { Button } from "react-native-elements"
+export default function Lista9({navigation, route}){
+    const ide = route.params
+    let id = "0000"
+    if (typeof(Object.values(Object.values(ide)[0])[0]) !== "undefined"){
+        id = (Object.values(Object.values(Object.values(ide)[0])[0])[0])
+    }
+    console.log(id)
+    const [listaP9, setListaP9] = useState("") 
+    const [ListaN9, setListaN9] = useState("Lista 9");
+
+    const añadir = () => {
+    if (id !== "0000"){
+        if (listaP9 !== ""){
+        var listasplit = listaP9.split(",")
+        if (listasplit.includes(id) === false){
+            var nueva = listaP9.concat(id +",")
+            setListaP9(nueva)
+        } else {
+            Alert.alert(
+                "No pudimos agregar tu Anime UnU",
+                "Este Anime ya esta en la lista, prueba con otro :3",
+            ) 
+        }
+    } else {
+        setListaP9(id +",")
+    }
+    }}
+
+    const storeData = async () => {
+        try {
+            console.log("Guardando")
+            await AsyncStorage.setItem("@Lista9", listaP9);
+        } catch (err) {
+            alert(err);
+        } 
+    }
+
+    const getData = async () => {
+        try{
+            console.log("Cargando")
+            let listaP9 = await AsyncStorage.getItem("@Lista9");
+            if (listaP9 !== null) {
+                setListaP9(listaP9);
+            }
+        } catch (err) {
+            alert(err)
+            }
 }
+    
+    const borrar = () => {
+        Alert.alert(
+            "¿Desea purgar esta lista?",
+            "Al purgar la lista se borraran todos los Animes que contiene.",
+            [
+              { text: "Cancelar", onPress: () => console.log("Cancel Pressed")},
+              { text: "Borrar", onPress: () => removeData() }
+            ],
+            { cancelable: false }
+          )
+    }
+
+    const removeData = async () => {
+        try {
+            console.log("Purgado")
+            await AsyncStorage.removeItem("@Lista9")
+    } catch (err) {
+        alert(err)
+    } finally {
+        setListaP9("");
+    };
+    };
+
+    const save = async() => {
+        try {
+            await AsyncStorage.setItem("Lista 9", ListaN9);
+        } catch (err) {
+            alert(err);
+        }
+    };
+
+    const load = async () => {
+        try{
+            let ListaN9 = await AsyncStorage.getItem("Lista 9");
+            if (ListaN9 !== null) {
+                setListaN9(ListaN9);
+            }
+        } catch (err) {
+            alert(err)
+        }
+    };
+
+    const remove = async () => {
+        try {
+            await AsyncStorage.removeItem("Lista 9")
+    } catch (err) {
+        alert(err)
+    } finally {
+        setListaN9("");
+    };
+    };
+    
+    useEffect(() => {
+        load();
+        getData();
+    },[]);
+
+    return(
+        <SafeAreaView style={styles.container}>
+        <Text style = {styles.titulo}> {ListaN9} </Text>
+        <TextInput 
+            style={styles.input}
+            placeholder = "Inserte Nombre Lista"
+            onChangeText = {(text) => setListaN9(text)}>
+        </TextInput>
+        <Button onPress={() => añadir()}
+            title = "Añadir"
+            type = "outline"
+         buttonStyle = {{
+            backgroundColor:"#dcdcdc",
+            marginHorizontal: 20,
+            borderColor: "white",   
+            borderRadius: 10, 
+            marginVertical: 10,                 
+        }}> </Button>
+        <Button onPress={() => storeData()}
+            title = "Guardar Anime"
+            type = "outline"
+         buttonStyle = {{
+            backgroundColor:"#dcdcdc",
+            marginHorizontal: 20,
+            borderColor: "white",   
+            borderRadius: 10, 
+            marginVertical: 10,                 
+        }}> </Button>
+        <Button onPress={() => borrar()}
+            title = "Purgar Lista"
+            type = "outline"
+         buttonStyle = {{
+            backgroundColor:"#dcdcdc",
+            marginHorizontal: 20,
+            borderColor: "white",   
+            borderRadius: 10,
+            marginVertical: 10,                  
+        }}> </Button>
+        <Button onPress={() => navigation.navigate("Ver Lista 9", {lista: listaP9})}
+            title = "Ver Lista"
+            type = "outline"
+         buttonStyle = {{
+            backgroundColor:"#dcdcdc",
+            marginHorizontal: 20,
+            borderColor: "white",   
+            borderRadius: 10,
+            marginVertical: 10,                
+        }}> </Button>
+        <Button onPress={() => save()}
+            title = "Guardar Nombre"
+            type = "outline"
+         buttonStyle = {{
+            backgroundColor:"#dcdcdc",
+            marginHorizontal: 20,
+            borderColor: "white",   
+            borderRadius: 10,
+            marginVertical: 10,                
+        }}> </Button>
+        </SafeAreaView>
+        )}
+    
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        input: {
+            marginBottom: "auto",
+            borderWidth: 1,
+            borderColor: "#777",
+            padding: 8,
+            margin: 10,
+            width: 200,
+            backgroundColor: "white",
+        },
+        listas: {
+            alignItems: "center",
+            backgroundColor: "#DDDDDD",
+            padding: 10,
+            marginBottom: 25,
+        },
+        titulo: {
+            marginTop:"auto"
+        }
+    })
