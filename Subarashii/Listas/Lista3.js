161e6af46d813d3587f16dd65569c8b4688b8744
Jose-Cancino <josemartin091101@gmail.com>
2020-12-21 15:56:20 -0300

    Update Lista3.js

--- Subarashii/Listas/Lista3.js
+++ Subarashii/Listas/Lista3.js
@@ -1,70 +1,197 @@
 import React, {useState, Component, useEffect} from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert, TouchableOpacity, SafeAreaView, ScrollView, TextPropTypes } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert, SafeAreaView, ScrollView, TextPropTypes } from 'react-native';
 import AsyncStorage from "@react-native-community/async-storage";
-import { createStackNavigator } from '@react-navigation/stack';
-import pagAnime from "../pagAnime"
-import RecPerso from '../ListasRec/RecPerso';
-import Recomendacion from '../Recomendacion';
+import { Button } from "react-native-elements"
+export default function Lista3({navigation, route}){
+    const ide = route.params
+    let id = "0000"
+    if (typeof(Object.values(Object.values(ide)[0])[0]) !== "undefined"){
+        id = (Object.values(Object.values(Object.values(ide)[0])[0])[0])
+    }
+    console.log(id)
+    const [listaP3, setListaP3] = useState("") 
+    const [ListaN3, setListaN3] = useState("Lista 3");
 
-export default function Lista3({navigation}){
+    const añadir = () => {
+    if (id !== "0000"){
+        if (listaP3 !== ""){
+        var listasplit = listaP3.split(",")
+        if (listasplit.includes(id) === false){
+            var nueva = listaP3.concat(id +",")
+            setListaP3(nueva)
+        } else {
+            Alert.alert(
+                "No pudimos agregar tu Anime UnU",
+                "Este Anime ya esta en la lista, prueba con otro :3",
+            ) 
+        }
+    } else {
+        setListaP3(id +",")
+    }
+    }}
 
-    const Stack = createStackNavigator();
-    <Stack.Navigator>
-        <Stack.Screen name= "Anime" component = {pagAnime} />
-        <Stack.Screen name= "RecPerso" component = {RecPerso}/>
-    </Stack.Navigator>
-    
-    const lista2 = ["41370", "5714","7442","6448","8133","41982","13569","42586","8671","8699"]
-    const Bloques = lista2.map(lista2 => {
-        const [nombre, setNombre] = useState("")
-        const [id, setId] = useState("")
-  
-          fetch(`https://kitsu.io/api/edge/anime/${lista2}`)
-          .then((response) => response.json())
-          .then((json) => {
-            setNombre(json.data.attributes.canonicalTitle);
-            setId(json.data.id)
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-          
-        return ( 
-          <View >
-            <TouchableOpacity onPress = {() => navigation.navigate("Anime", {id: `${id}`})}>
-              <Text style={styles.lista}>{nombre}</Text>
-            </TouchableOpacity>
-          </View>
-          );
+    const storeData = async () => {
+        try {
+            console.log("Guardando")
+            await AsyncStorage.setItem("@Lista3", listaP3);
+        } catch (err) {
+            alert(err);
+        } 
+    }
 
-        });
-   
-    return(
-    
-    <ScrollView>
-        <Button onPress={() => navigation.navigate("RecPerso", {lista2})} title = "Recomendacion">
-        </Button>
-
-        <View>
-            {Bloques}
-        </View>
-                
-    </ScrollView>
-    )
+    const getData = async () => {
+        try{
+            console.log("Cargando")
+            let listaP3 = await AsyncStorage.getItem("@Lista3");
+            if (listaP3 !== null) {
+                setListaP3(listaP3);
+            }
+        } catch (err) {
+            alert(err)
+            }
 }
+    
+    const borrar = () => {
+        Alert.alert(
+            "¿Desea purgar esta lista?",
+            "Al purgar la lista se borraran todos los Animes que contiene.",
+            [
+              { text: "Cancelar", onPress: () => console.log("Cancel Pressed")},
+              { text: "Borrar", onPress: () => removeData() }
+            ],
+            { cancelable: false }
+          )
+    }
+
+    const removeData = async () => {
+        try {
+            console.log("Purgado")
+            await AsyncStorage.removeItem("@Lista3")
+    } catch (err) {
+        alert(err)
+    } finally {
+        setListaP3("");
+    };
+    };
+
+    const save = async() => {
+        try {
+            await AsyncStorage.setItem("Lista 3", ListaN3);
+        } catch (err) {
+            alert(err);
+        }
+    };
+
+    const load = async () => {
+        try{
+            let ListaN3 = await AsyncStorage.getItem("Lista 3");
+            if (ListaN3 !== null) {
+                setListaN3(ListaN3);
+            }
+        } catch (err) {
+            alert(err)
+        }
+    };
+
+    const remove = async () => {
+        try {
+            await AsyncStorage.removeItem("Lista 3")
+    } catch (err) {
+        alert(err)
+    } finally {
+        setListaN3("");
+    };
+    };
+    
+    useEffect(() => {
+        load();
+        getData();
+    },[]);
+
+    return(
+    <SafeAreaView style={styles.container}>
+    <Text style = {styles.titulo}> {ListaN3} </Text>
+    <TextInput 
+        style={styles.input}
+        placeholder = "Inserte Nombre Lista"
+        onChangeText = {(text) => setListaN3(text)}>
+    </TextInput>
+    <Button onPress={() => añadir()}
+        title = "Añadir"
+        type = "outline"
+     buttonStyle = {{
+        backgroundColor:"#dcdcdc",
+        marginHorizontal: 20,
+        borderColor: "white",   
+        borderRadius: 10, 
+        marginVertical: 10,                 
+    }}> </Button>
+    <Button onPress={() => storeData()}
+        title = "Guardar Anime"
+        type = "outline"
+     buttonStyle = {{
+        backgroundColor:"#dcdcdc",
+        marginHorizontal: 20,
+        borderColor: "white",   
+        borderRadius: 10, 
+        marginVertical: 10,                 
+    }}> </Button>
+    <Button onPress={() => borrar()}
+        title = "Purgar Lista"
+        type = "outline"
+     buttonStyle = {{
+        backgroundColor:"#dcdcdc",
+        marginHorizontal: 20,
+        borderColor: "white",   
+        borderRadius: 10,
+        marginVertical: 10,                  
+    }}> </Button>
+    <Button onPress={() => navigation.navigate("Ver Lista 3", {lista: listaP3})}
+        title = "Ver Lista"
+        type = "outline"
+     buttonStyle = {{
+        backgroundColor:"#dcdcdc",
+        marginHorizontal: 20,
+        borderColor: "white",   
+        borderRadius: 10,
+        marginVertical: 10,                
+    }}> </Button>
+    <Button onPress={() => save()}
+        title = "Guardar Nombre"
+        type = "outline"
+     buttonStyle = {{
+        backgroundColor:"#dcdcdc",
+        marginHorizontal: 20,
+        borderColor: "white",   
+        borderRadius: 10,
+        marginVertical: 10,                
+    }}> </Button>
+    </SafeAreaView>
+    )}
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-    lista:{
-      marginHorizontal: 20,
-      marginTop: 24,
-      padding: 30,
-      backgroundColor: "pink",
-      fontSize: 24,
+    input: {
+        marginBottom: "auto",
+        borderWidth: 1,
+        borderColor: "#777",
+        padding: 8,
+        margin: 10,
+        width: 200,
+        backgroundColor: "white",
     },
-  });
+    listas: {
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10,
+        marginBottom: 25,
+    },
+    titulo: {
+        marginTop:"auto"
+    }
+})
