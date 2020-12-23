7e4ff11e28a7f422e404f83a3b3cfb0a34117996
Javiera9920 <74120075+Javiera9920@users.noreply.github.com>
2020-12-23 19:55:49 -0300

    Update Contenido.js

--- Subarashii/TodasCategorias/Contenido.js
+++ Subarashii/TodasCategorias/Contenido.js
@@ -1,25 +1,32 @@
 import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native';
 import { StyleSheet,
     View,
-
+    ImageBackground,
  } from 'react-native';
  import { Button } from "react-native-elements"
+
+ const image = {uri:"https://hbimg.huabanimg.com/20800fec57d23058fd04d16ebd0465fff898f4aa49bd26-nCYzlY"};
  const Contenido = ({navigation}) => {
     return(
+      <SafeAreaView>
+        <ImageBackground source={image} style={styles.image}></ImageBackground>
         <View>
             <Button 
                 onPress={() => navigation.navigate("listaCategoria", {categoria: "violence"} )}
                 title = "Violence"
                 type = "outline"
                 buttonStyle = {{
-                    backgroundColor:"white",
+                    backgroundColor:'rgba(255, 255, 255, 0.9)',
                     marginHorizontal: 20,
                     borderColor: "white",
                     borderRadius: 10,
+                    borderWidth: 2,
                     marginVertical: 15,
                   }}
             ></Button>
         </View>
+        </SafeAreaView>
     )
 
 
@@ -36,7 +43,12 @@ import { StyleSheet,
       backgroundColor: "#DDDDDD",
       padding: 10,
       marginBottom: 15,
-    }
+    },
+    image: {
+      flex: 1,
+      resizeMode:"contain",
+      height: 800     
+    },
   
   });
  export default Contenido;
