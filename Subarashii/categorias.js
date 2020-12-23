3b4a9e9701ce7233bcf43c6602c5c589ce5ebb9d
Javiera9920 <74120075+Javiera9920@users.noreply.github.com>
2020-12-23 03:12:10 -0300

    Update categorias.js

--- Subarashii/categorias.js
+++ Subarashii/categorias.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet,
+    ImageBackground,
     Text,
     View,
     Image,
@@ -12,15 +13,20 @@ import { StyleSheet,
     StatusBar 
  } from 'react-native';
  import { Button } from "react-native-elements"
+
+ const image = {uri:"https://i.pinimg.com/564x/32/db/75/32db75e9d1036f891343ece7e8bb555c.jpg"};
+
 const categorias = ({navigation}) => {
     return(
+    <SafeAreaView>
+      <ImageBackground source={image} style={styles.image}></ImageBackground>     
         <ScrollView>
             <Button 
                 onPress={() => navigation.navigate("Contenido")}
                 title = "Contenido"
                 type = "outline"
                 buttonStyle = {{
-                    backgroundColor:"white",
+                    backgroundColor: 'white',
                     marginHorizontal: 20,
                     borderColor: "white",
                     borderRadius: 10,
@@ -54,7 +60,7 @@ const categorias = ({navigation}) => {
             ></Button>
             <Button
                 onPress={() => navigation.navigate("Publico")}
-                title = "Publico"
+                title = "Público"
                 type = "outline"
                 buttonStyle = {{
                     backgroundColor:"white",
@@ -66,7 +72,7 @@ const categorias = ({navigation}) => {
             ></Button>
             <Button 
                 onPress={() => navigation.navigate("Tematica")}
-                title = "Tematicas"
+                title = "Temática"
                 type = "outline"
                 buttonStyle = {{
                     backgroundColor:"white",
@@ -77,6 +83,7 @@ const categorias = ({navigation}) => {
                   }}
             ></Button>
         </ScrollView>
+      </SafeAreaView>
     )
 
 
@@ -87,6 +94,11 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    image: {
+      flex: 1,
+      resizeMode:"contain",
+      height: 800     
     },
     listas: {
       alignItems: "center",
