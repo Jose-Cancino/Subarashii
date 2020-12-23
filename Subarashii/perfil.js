6d7e99d5e2c5b6f6bad895e2c53f7adc9ae09b8b
Javiera9920 <74120075+Javiera9920@users.noreply.github.com>
2020-12-23 03:13:16 -0300

    Update perfil.js

--- Subarashii/perfil.js
+++ Subarashii/perfil.js
@@ -1,21 +1,200 @@
-import React from "react";
-import {Text, StyleSheet} from "react-native";
+import React, {useState, Component, useEffect} from 'react';
+import { StyleSheet, Text, View, TextInput, Alert, SafeAreaView, ScrollView, TextPropTypes, ImageBackground} from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import AsyncStorage from "@react-native-community/async-storage";
+import { Avatar, Button } from "react-native-elements";
+import vistos from "./Listas/Vistos"
+import viendo from "./Listas/Viendo"
+import porver from "./Listas/Porver"
+import ListaPersoVistos from "./Listas/ListaPersoVistos"
+import ListaPersoViendo from "./Listas/ListaPersoViendo"
+import ListaPersoVer from "./Listas/ListaPersoPorver"
+import pagAnime from "./pagAnime"
+import RecPerso from "./ListasRec/RecPerso"
 
-export default function perfil(){
-    return(
-    <Text style={styles.title}>
-        TU PERFIL
-    </Text>
-    )}
+const image = {uri:"https://i.pinimg.com/originals/72/42/12/7242120be58db412e64993dd41e4fa2e.jpg"};
+const Perfil = ({route, navigation}) => {
+    const id = route.params;
+    const [nombre, setNombre] = useState("")
+    const [casinombre, setCasinombre] = useState("")
+    const [casimagen, setCasimagen] = useState("xd")
+    const [imagen, setImagen] = useState("xd")
 
+    const storeData = async () => {
+        try {
+            await AsyncStorage.setItem("@imagenp", casimagen);
+        } catch (err) {
+            alert(err);
+        } console.log("Guardando")
+    }
+
+    const save = async () => {
+        try {
+            await AsyncStorage.setItem("@name", casinombre);
+        } catch (err) {
+            alert(err);
+        } console.log("Guardando")
+    }
+
+    const load = async () => {
+        try{
+            console.log("Cargando")
+            let nombre = await AsyncStorage.getItem("@name");
+            if (nombre !== null) {
+                setNombre(nombre);
+            }
+        } catch (err) {
+            alert(err)
+            }
+}
+
+    const getData = async () => {
+        try{
+            console.log("Cargando")
+            let imagen = await AsyncStorage.getItem("@imagenp");
+            if (imagen !== null) {
+                setImagen(imagen);
+            }
+        } catch (err) {
+            alert(err)
+            }
+}
+
+useEffect(() => {
+    getData();
+    load()
+},[]);
+
+return(
+    <SafeAreaView>
+    <ImageBackground source={image} style={styles.image}></ImageBackground>
+    <Avatar
+        containerStyle ={{
+            alignSelf: "center",
+            marginTop: 50,
+            borderWidth: 3,
+            borderColor: 'white'
+        }}
+        size = "xlarge"
+        rounded
+        source={{
+        uri:
+            `${imagen}`,
+        }}/>
+    <Text> {nombre} </Text>
+    <TextInput 
+        style={styles.input}
+        placeholder = "Inserte URL Imagen"
+        onChangeText = {(text) => setCasimagen(text)}>
+    </TextInput>
+
+    <TextInput 
+        style={styles.input}
+        placeholder = "Inserte Nombre"
+        onChangeText = {(text) => setCasinombre(text)}>
+    </TextInput>
+
+    <Button onPress={() => {storeData(); setImagen(casimagen)}}
+        title = "Añadir"
+        type = "outline"
+     buttonStyle = {{
+        backgroundColor:"'rgba(255, 255, 255, 0.8)'",
+        marginHorizontal: 20,
+        borderColor: "white",   
+        borderRadius: 10, 
+        marginVertical: 10,                 
+    }}> </Button>
+    <Button onPress={() => {save(); setNombre(casinombre)}}
+        title = "Guardar nombre"
+        type = "outline"
+     buttonStyle = {{
+        backgroundColor:"'rgba(255, 255, 255, 0.8)'",
+        marginHorizontal: 20,
+        borderColor: "white",   
+        borderRadius: 10, 
+        marginVertical: 10,                 
+    }}> </Button>
+    <Button onPress={() => navigation.navigate("Vistos", {id: {id}})}
+        title = "Vistos"
+        type = "outline"
+     buttonStyle = {{
+        backgroundColor:"'rgba(255, 255, 255, 0.8)'",
+        marginHorizontal: 20,
+        borderColor: "white",   
+        borderRadius: 10, 
+        marginVertical: 10,                 
+    }}> </Button>
+    <Button onPress={() => navigation.navigate("Viendo", {id: {id}})}
+        title = "Viendo"
+        type = "outline"
+     buttonStyle = {{
+        backgroundColor:"'rgba(255, 255, 255, 0.8)'",
+        marginHorizontal: 20,
+        borderColor: "white",   
+        borderRadius: 10, 
+        marginVertical: 10,                 
+    }}> </Button>
+    <Button onPress={() => navigation.navigate("Por ver", {id: {id}})}
+        title = "Por ver"
+        type = "outline"
+     buttonStyle = {{
+        backgroundColor:"'rgba(255, 255, 255, 0.8)'",
+        marginHorizontal: 20,
+        borderColor: "white",   
+        borderRadius: 10, 
+        marginVertical: 10,                 
+    }}> </Button>
+    </SafeAreaView>
+    )
+
+}
+
+
+const Stack = createStackNavigator();
+
+function perfil() {
+    return (
+<Stack.Navigator initialRouteName = "Perfil">
+    <Stack.Screen name = "Perfil" component = {Perfil} />
+    <Stack.Screen name = "Vistos" component = {vistos} />
+    <Stack.Screen name = "Viendo" component = {viendo} />
+    <Stack.Screen name = "Por ver" component = {porver} />
+    <Stack.Screen name = "Ver Lista Vistos" component = {ListaPersoVistos} options = {{title: "Los que ya vi"}}/>
+    <Stack.Screen name = "Ver Lista Viendo" component = {ListaPersoViendo} options = {{title: "Los que estoy viendo"}}/>
+    <Stack.Screen name = "Ver Lista Ver" component = {ListaPersoVer} options = {{title: "Los que quiero ver"}}/>
+    <Stack.Screen name= "Anime" component = {pagAnime} />
+    <Stack.Screen name= "RecPerso" component = {RecPerso} options = {{title: "Recomendación Personalizada"}}/>
+</Stack.Navigator>
+)}
 
 const styles = StyleSheet.create({
-title: {
-    marginVertical: 50,
-    alignItems: 'center',
-    textAlign: 'center',
-    fontSize:13,
-    fontWeight: 'bold',
-    color: 'black',
-  }
+    container:{
+        alignItems: "center",
+        marginVertical: 5,
+    },
+    title: {
+        marginVertical: 50,
+        alignItems: 'center',
+        textAlign: 'center',
+        fontSize:13,
+        fontWeight: 'bold',
+        color: 'black',
+    },
+    input: {
+        marginBottom: "auto",
+        borderWidth: 1,
+        borderColor: "#777",
+        padding: 8,
+        margin: 10,
+        width: 200,
+        marginHorizontal: 100,
+        backgroundColor: "white",
+    },
+    image: {
+        flex: 1,
+        resizeMode:"contain",
+        height: 800     
+      },
 })
+
+export default perfil;
